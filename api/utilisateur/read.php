04b5657da510f3c56fb5e27ade0741193ce28ce4
Houssamer <riad.houssame.eleve@ehtp.ac.ma>
2022-01-04 19:10:30 +0000

    Auth

--- api/utilisateur/read.php
+++ api/utilisateur/read.php
@@ -1,8 +1,10 @@
 <?php 
 
     // Headers 
-    header('Access-Control-Allow-Origin: *'); 
-    header('Content-Type: application/json'); 
+    header('Access-Control-Allow-Origin: *');
+    header('Content-Type: application/json');
+    header('Access-Control-Allow-Methods: GET');
+    header('Access-Control-Allow-Headers: Access-Control-Allow-Headers, Content-Type, Access-Control-Allow-Methods, Authorization, X-Requested-With');
 
     include_once '../../config/Database.php';
     include_once '../../models/Utilisateur.php';
